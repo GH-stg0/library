55d706e8a8f9db88f73df1a44c698cae06975362
Kentaro Ninose <176794298+GH-stg0@users.noreply.github.com>
2025-07-14 11:19:09 +0900

    [Update] UnionFind.cpp

--- DataStructure/UnionFind.cpp
+++ DataStructure/UnionFind.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 using ll = long long;
 
-class UnionFind {
-    private:
+struct UnionFind {
+private:
+    int n;
     vector<int> parent, siz;
 
     public:
-    UnionFind(size_t n) {
+    explicit UnionFind(int N) : n(N) {
         parent.resize(n);
         siz.resize(n, 1);
         for (int i = 0; i < n; i++) parent[i] = i;
     }
 
     int find(int x) {
+        assert(0 <= x && x < n);
         if (parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
 
     void unite(int x, int y) {
+        assert(0 <= x && x < n);
+        assert(0 <= y && y < n);
         int rootX = find(x);
         int rootY = find(y);
         if (rootX == rootY) return;
@@ -30,10 +34,36 @@ class UnionFind {
     }
 
     int size(int x) {
+        assert(0 <= x && x < n);
         return siz[find(x)];
     }
 
     bool same(int x, int y) {
+        assert(0 <= x && x < n);
+        assert(0 <= y && y < n);
         return (find(x) == find(y));
+    }
+
+    int count(void) {
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == find(i)) ret++;
+        }
+        return ret;
+    }
+
+    void debug(void) {
+        vector<vector<int>> group(n);
+        for (int i = 0; i < n; i++) group[find(i)].emplace_back(i);
+        for (int i = 0; i < n; i++) {
+            if (!group[i].empty()) {
+                cout << i << " : {";
+                for (int j = 0; j < siz[i]; j++) {
+                    if (j > 0) cout << ", ";
+                    cout << group[i][j];
+                }
+                cout << "}" << endl;
+            }
+        }
     }
 };
